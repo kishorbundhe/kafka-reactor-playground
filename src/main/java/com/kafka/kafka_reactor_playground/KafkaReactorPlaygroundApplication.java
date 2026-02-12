494a3df054ce12c51da053ebc8d9763efeda5f7d
Kishor Bundhe <kishorbundhe@gmail.com>
2026-02-13 00:39:07 +0100

    init commit

---
+++ src/main/java/com/kafka/kafka_reactor_playground/KafkaReactorPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.kafka_reactor_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaReactorPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaReactorPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/kafka_reactor_playground/KafkaReactorPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.kafka_reactor_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaReactorPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
